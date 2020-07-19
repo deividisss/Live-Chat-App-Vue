0844d975617cd8be20fa5d21d67b00cc8184119f
Deividas <deivis124@gmail.com>
2020-07-20 01:07:08 +0300

    Adds Chat component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Welcome from "@/components/Welcome";
 
 Vue.use(Router);
+import Chat from "@/components/Chat";
 
 export default new Router({
   routes: [
@@ -10,6 +11,12 @@ export default new Router({
       path: "/",
       name: "Welcome",
       component: Welcome
+    },
+    {
+      path: "/chat",
+      name: "Chat",
+      component: Chat,
+      props: true
     }
   ]
 });
